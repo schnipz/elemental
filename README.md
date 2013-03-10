44ac90b8d634c966c1512e177769a8669ebe66cf
Mike Parsons <info@schnipz.co.uk>
2013-03-10 13:04:17 +0000

    Major file structure update for version 0.2
    Now using SCSS as well as a proper site directory structure for quicker
    use.

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-Elemental 0.1
-=========
+Elemental
+============
 A scratch build start point for front-end developers.
 
 This framework has been built up over the years as simple starting point to scratch build from.
 
 Please feel free to do the same!
+
+Requirements
+------------
+
+None
+
+License
+------------
+
+Copyright (c) 2013, Mike Parsons
+
+Licensed under the [MIT license](https://github.com/schnipz/elemental/blob/master/LICENSE.txt).
---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+/*
+ * Elemental v0.2 - scripts.js (https://github.com/schnipz/elemental)
+ * http://www.schnipz.co.uk
+ * Copyright (C) 2013, Mike Parsons
+ * MIT License
+ */
+
+// NOTE: Change "EXAMPLE" to suit your project.
+
+EXAMPLE = {
+	init: function(){
+		// Executed on document ready...
+		ELEMENTAL.FUNCTIONS.DEBUG.loadTime('ELEMENTAL.init');
+
+		// Bind window resize events.
+		var resizeTimer;
+		window.onresize(function(){
+			clearTimeout(resizeTimer);
+			resizeTimer = setTimeout(EXAMPLE.resize, 200);
+		});
+
+		// Begin JS...
+
+	},
+	resize: function(){
+		// Executed on window resize...
+		ELEMENTAL.FUNCTIONS.DEBUG.loadTime('ELEMENTAL.resize');
+
+	}
+};
+
+//---[ HERE WE GO... ]--------------------------------------------------------//
+$(document).ready(function(){
+	ELEMENTAL.FUNCTIONS.DEBUG.loadTime('$(document).ready');
+	EXAMPLE.init();
+});
+//----------------------------------------------------------------------------//
