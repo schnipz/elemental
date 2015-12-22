e71924747b9ab78bf742ca2fc4549512955531a0
Mike Parsons <schnipz@gmail.com>
2015-12-22 17:41:05 +0000

    Add SCSS build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
 var gulp = require('gulp');
-var shell = require('gulp-shell');
+var sass = require('gulp-sass');
 var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
+var shell = require('gulp-shell');
 
-gulp.task('scripts', function() {
+gulp.task('styles', function() {
+  gulp.src('src/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('dist/includes/css/'));
+});
+
+gulp.task('lint-scripts', function() {
   gulp.src(['src/js/**/*.js', '!src/js/build.js', '!src/js/imports/third-party/**/*.js'])
     .pipe(jscs())
     .pipe(jscs.reporter())
     .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'));
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('build-js', shell.task([
+gulp.task('scripts', ['lint-scripts'], shell.task([
   'r.js -o src/js/build.js'
 ]));
 
-gulp.task('develop', ['scripts', 'build-js'], function() {
-  gulp.watch(['src/js/**/*.js', '!src/js/imports/third-party/**/*.js'], ['scripts', 'build-js']);
+gulp.task('develop', ['styles', 'scripts'], function() {
+  gulp.watch(['src/scss/**/*.scss'], ['styles']);
+  gulp.watch(['src/js/**/*.js', '!src/js/imports/third-party/**/*.js'], ['scripts']);
 });
